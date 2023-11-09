81b2473aecc41aab8589f08d42d431bc46317621
Lucas <lucas_river_2814@hotmail.com>
2023-11-09 09:25:51 -0300

    End of exercise 3.8

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+
 app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms")
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 app.use(express.json());
 
