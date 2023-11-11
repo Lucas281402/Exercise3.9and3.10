5f726f41645ccdbbe44f4b062c666e9b5b285ed2
Lucas <lucas_river_2814@hotmail.com>
2023-11-11 10:23:52 -0300

    End of exercise 3.13 and beginning of exercise 3.14

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require('cors')
+require('dotenv').config()
+const Person = require('./models/person')
 
 const app = express();
 
@@ -13,35 +15,10 @@ app.use(
 );
 app.use(express.json());
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-const generateID = () => {
-  return Math.floor(Math.random() * 1000000) + 1;
-};
-
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 });
 
 app.get("/api/info", (request, response) => {
